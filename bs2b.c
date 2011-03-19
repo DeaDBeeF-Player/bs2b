a1961cd2f0686a7bdf0915f1fc7d62b5aba369bd
Steven McDonald <steven@steven-mcdonald.id.au>
2011-03-19 17:37:00 +1100

    Finally got a working 0.5 port

--- bs2b.c
+++ bs2b.c
@@ -7,19 +7,13 @@
 #include <deadbeef/deadbeef.h>
 #include <bs2b/bs2b.h>
 #include <stdlib.h>
+#include <string.h>
 
 //#define trace(...) { fprintf(stderr, __VA_ARGS__); }
 #define trace(fmt,...)
 
 static DB_dsp_t plugin;
 DB_functions_t *deadbeef;
-
-/*
-static t_bs2bdp dp = 0;
-static short enabled = 0;
-static uint32_t level = ( (uint32_t)700 | ((uint32_t)45 << 16) );
-static int savedrate = 44100;
-*/
 
 enum {
     BS2B_PARAM_XFEED = 0,
@@ -36,28 +30,6 @@ typedef struct {
     int savedrate;
 } ddb_bs2b_t;
 
-//void
-//ddb_bs2b_reset (void);
-
-/* int
-bs2b_on_configchanged (DB_event_t *ev, uintptr_t data) {
-    int e = deadbeef->conf_get_int ("bs2b.enable", 0);
-    if (e != enabled) {
-        if (e) {
-            bs2b_reset ();
-        }
-        enabled = e;
-    }
-
-    uint32_t l = ( ((uint32_t)deadbeef->conf_get_int ("bs2b.fcut", 700)) | ((uint32_t)(deadbeef->conf_get_float ("bs2b.feed", 4.5) * 10) << 16) );
-    if (l != level) {
-        bs2b_set_level (dp, l);
-        level = l;
-    }
-    
-    return 0;
-} */
-
 ddb_dsp_context_t*
 ddb_bs2b_open (void) {
     ddb_bs2b_t *bs2b = malloc (sizeof (ddb_bs2b_t));
@@ -67,7 +39,6 @@ ddb_bs2b_open (void) {
     bs2b->cutoff = 45;
     bs2b->savedrate = 44100;
     bs2b->dp = bs2b_open ();
-//    bs2b_set_level (dp, level);
     return (ddb_dsp_context_t *)bs2b;
 }
 
@@ -97,18 +68,6 @@ ddb_bs2b_reset (ddb_dsp_context_t *ctx) {
     return;
 }
 
-/* void
-bs2b_enable (int e) {
-    if (e != enabled) {
-        deadbeef->conf_set_int ("bs2b.enable", e);
-        if (e && !enabled) {
-            bs2b_reset ();
-        }
-        enabled = e;
-    }
-    return;
-}*/
-
 int
 ddb_bs2b_num_params (void) {
     return BS2B_PARAM_COUNT;
@@ -123,6 +82,7 @@ ddb_bs2b_get_param_name (int p) {
         return "Cutoff filter (Hz)";
     default:
         fprintf (stderr, "ddb_bs2b_get_param_name: invalid param index (%d)\n", p);
+        return "";
     }
 }
 
@@ -132,16 +92,21 @@ ddb_bs2b_set_param (ddb_dsp_context_t *_bs2b, int p, const char *val) {
 
     switch (p) {
     case BS2B_PARAM_XFEED:
-        bs2b->xfeed = (uint16_t)(atof (val) * 10);
-        break;
+        trace ("ddb_bs2b_set_param: xfeed set to %s\n", val);
+        bs2b->xfeed = (uint16_t)(atof (val) * 100);
+        // returning here needed to stop bs2b_set_level being called twice
+        // should probably come up with a less hacky solution
+        return; // break;
     case BS2B_PARAM_CUTOFF:
-        bs2b->cutoff = atoi (val);
+        trace ("ddb_bs2b_set_param: cutoff set to %s\n", val);
+        bs2b->cutoff = (uint16_t)(atof (val) / 10);
         break;
     default:
         fprintf (stderr, "ddb_bs2b_set_param: invalid param index (%d)\n", p);
         return;
     }
 
+    trace ("ddb_bs2b_set_param: bs2b_set_level called with %d, %d)\n", (uint32_t)bs2b->xfeed, (uint32_t)bs2b->cutoff);
     bs2b_set_level (bs2b->dp, (uint32_t)bs2b->xfeed | ((uint32_t)bs2b->cutoff << 16));
 }
 
@@ -151,10 +116,10 @@ ddb_bs2b_get_param (ddb_dsp_context_t *_bs2b, int p, char *val, int sz) {
 
     switch (p) {
     case BS2B_PARAM_XFEED:
-        snprintf (val, sz, "%f", (float)bs2b->xfeed / 10);
+        snprintf (val, sz, "%f", (float)bs2b->xfeed / 100);
         break;
     case BS2B_PARAM_CUTOFF:
-        snprintf (val, sz, "%d", bs2b->cutoff);
+        snprintf (val, sz, "%f", 10*(float)bs2b->cutoff);
         break;
     default:
         fprintf (stderr, "ddb_bs2b_get_param: invalid param index (%d)\n", p);
@@ -162,7 +127,6 @@ ddb_bs2b_get_param (ddb_dsp_context_t *_bs2b, int p, char *val, int sz) {
 }
 
 static const char ddb_bs2b_dialog[] =
-//    "property \"Enable\" checkbox bs2b.enable 0;\n"
     "property \"Crossfeed level (dB)\" hscale[1.0,15.0,0.1] 0 4.5;\n"
     "property \"Cutoff filter (Hz)\" hscale[100,2000,1] 1 700;\n"
 ;
@@ -175,8 +139,7 @@ static DB_dsp_t plugin = {
     .plugin.id = "bs2b",
     .plugin.name = "Headphone crossfeed",
     .plugin.descr = "Headphone crossfeed plugin using libbs2b by Boris Mikhaylov",
-    .plugin.author = "Steven McDonald",
-    .plugin.email = "steven.mcdonald@libremail.me",
+    .plugin.copyright = "Copyright (C) 2010-2011 Steven McDonald <steven.mcdonald@libremail.me>",
     .plugin.website = "http://gitorious.org/deadbeef-sm-plugins/pages/Home",
     .num_params = ddb_bs2b_num_params,
     .get_param_name = ddb_bs2b_get_param_name,
